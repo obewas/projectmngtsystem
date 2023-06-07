c2455d7039b13074e8cc4eb9cadcd5f7477d30b9
obewas <obewas1202@gmail.com>
2023-06-07 04:44:34 +0300

    added task details

--- projects/urls.py
+++ projects/urls.py
@@ -6,6 +6,10 @@ from . import views
 urlpatterns = [
   path('projects/', views.projectList, name = 'projects'),
   path('home/', views.home, name = 'home'),
+  path('projects/<int:pk>', views.projectDetail, name ='project-detail'), 
+  path('tasks/', views.taskList, name ='tasks'),
+  path('tasks/<int:pk>', views.taskDetail, name ='task-detail'),
+
  
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 from .models import Task, Project
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 def currentTime(request):
 	current_date_time = datetime.datetime.now()
@@ -18,5 +20,22 @@ def projectList(request):
    context = {'projects':projects}
    return render(request, 'projects.html',context)
 
+def projectDetail(request,pk):
+   project = get_object_or_404(Project, id=pk)
+   project_tasks = project.task_set.all()
+   
+   context = {'project':project,'project_tasks':project_tasks}
+   return render(request, 'project-detail.html',context)
 
 
+def taskList(request):
+    user_tasks =Task.objects.filter(assignee=request.user)
+    tasks = Task.objects.filter(assignee=None)
+ 
+    context = {'tasks':tasks,'user_tasks':user_tasks}
+    return render(request, 'tasks.html',context)
+
+def taskDetail(request,pk):
+    task = get_object_or_404(Task, id=pk)
+    context = {'task':task}
+    return render(request, 'task-detail.html',context)
